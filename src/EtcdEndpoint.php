0a1eef76fd7499580fb390450296c52a122574f9
s1lver <s1lverhron@gmail.com>
2023-04-13 23:32:15 +0300

    Added EtcdEndpoint

---
+++ src/EtcdEndpoint.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace S1lver\Etcd;
+
+final class EtcdEndpoint
+{
+    // Common
+
+    public const VERSION = '/version';
+
+    // KV
+
+    public const PUT = '/kv/put';
+    public const RANGE = 'kv/range';
+
+}
