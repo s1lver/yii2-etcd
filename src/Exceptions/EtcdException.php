dc1fdba5010aaa0c5fc90c61fbfd7d822ac48147
s1lver <s1lverhron@gmail.com>
2023-05-23 23:34:41 +0300

    First implement work with grpc

---
+++ src/Exceptions/EtcdException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace S1lver\Etcd\Exceptions;
+
+use Exception;
+
+class EtcdException extends Exception
+{}
--- src/RPC/EtcdGrpcModel.php
+++ src/RPC/EtcdGrpcModel.php
@@ -5,15 +5,87 @@ declare(strict_types=1);
 namespace S1lver\Etcd\RPC;
 
 use Etcd\KVClient;
+use Etcd\PutRequest;
+use Etcd\RangeRequest;
 use Grpc\ChannelCredentials;
 use S1lver\Etcd\EtcdServiceInterface;
+use S1lver\Etcd\Exceptions\EtcdException;
+use S1lver\Etcd\Rest\RangeResponse;
+use const Grpc\STATUS_OK;
 
 class EtcdGrpcModel implements EtcdServiceInterface
 {
+    public string $host = '';
+    public string $user = '';
+    public string $password = '';
+    public array $clientOptions = [];
 
-    private function getClient(string $host)
+    private KVClient $client;
+
+    /**
+     * @param string $host
+     * @param string $user
+     * @param string $password
+     * @param array $clientOptions
+     */
+    public function __construct(string $host, string $user, string $password, array $clientOptions)
     {
-        return new KVClient($host, [
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->clientOptions = $clientOptions;
+
+        $this->client = $this->getClient();
+    }
+
+    public function getVersion(): string
+    {
+        return 'Not supported';
+    }
+
+    public function getRange(string $key, string $rangeEnd): RangeResponse
+    {
+        $request = new RangeRequest();
+        $request->setKey($key);
+        $request->setRangeEnd($rangeEnd);
+
+        /** @var \Etcd\RangeResponse $response */
+        [$response, $status] = $this->client->Range($request)->wait();
+
+        if (STATUS_OK !== $status) {
+            throw new EtcdException('');
+        }
+    }
+
+    public function getKey(string $key): RangeResponse
+    {
+        $request = new RangeRequest();
+        $request->setKey($key);
+
+        /** @var \Etcd\RangeResponse $response */
+        [$response, $status] = $this->client->Range($request)->wait();
+
+        if (STATUS_OK !== $status) {
+            throw new EtcdException('');
+        }
+
+        $response->getKvs();
+    }
+
+    public function put(string $key, string $value): bool
+    {
+        $request = new PutRequest();
+        $request->setKey($key);
+        $request->setValue($value);
+
+        [$response, $status] = $this->client->Put($request)->wait();
+
+        return STATUS_OK === $status;
+    }
+
+    private function getClient(): KVClient
+    {
+        return new KVClient($this->host, [
             'credentials' => ChannelCredentials::createInsecure(),
         ]);
     }
